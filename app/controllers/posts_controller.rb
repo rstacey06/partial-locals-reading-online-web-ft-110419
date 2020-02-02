7e32b4bba88f92df738c87a70607e47384c636ec
github username <github email address>
2020-02-02 11:19:14 -0500

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,8 +1,7 @@
 class PostsController < ApplicationController
   def show
-    @post = Post.find(params[:id])
-    @author = @post.author
-  end
+      @post = Post.find(params[:id])
+    end
 
   def index
     @posts = Post.all
